52e8efeab0b96adadbd7809824622e0fe2af8622
Noam <noamvak765@gmail.com>
2020-03-04 23:47:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task01ex03/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.task01ex03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.btn);
+    }
+int num=0;
+    public void Click(View view) {
+        num++;
+        btn.setText("This is a click number:"+num);
+        if (num>6){
+            btn.setText("Enough to click. Go to new start!");
+            num=0;
+        }
+    }
+}
